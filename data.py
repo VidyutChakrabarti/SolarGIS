48da4c50ac82c96027a92340029f795e47529832
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-09-24 18:13:12 +0530

    Error correction

---
+++ data.py
@@ -0,0 +1,280 @@
+pv_data = {
+  'estimated_actuals': [
+    {
+      "pv_estimate": 2.48,
+      "period_end": "2024-09-16T07:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.563,
+      "period_end": "2024-09-16T07:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.558,
+      "period_end": "2024-09-16T06:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.51,
+      "period_end": "2024-09-16T06:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.402,
+      "period_end": "2024-09-16T05:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.287,
+      "period_end": "2024-09-16T05:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 2.11,
+      "period_end": "2024-09-16T04:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.888,
+      "period_end": "2024-09-16T04:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.607,
+      "period_end": "2024-09-16T03:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.182,
+      "period_end": "2024-09-16T03:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.849,
+      "period_end": "2024-09-16T02:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.537,
+      "period_end": "2024-09-16T02:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.196,
+      "period_end": "2024-09-16T01:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.006,
+      "period_end": "2024-09-16T01:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-16T00:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-16T00:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T23:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T23:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T22:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T22:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T21:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T21:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T20:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T20:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T19:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T19:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T18:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T18:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T17:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T17:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T16:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T16:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T15:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T15:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T14:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T14:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T13:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0,
+      "period_end": "2024-09-15T13:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.125,
+      "period_end": "2024-09-15T12:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.469,
+      "period_end": "2024-09-15T12:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 0.822,
+      "period_end": "2024-09-15T11:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.161,
+      "period_end": "2024-09-15T11:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.455,
+      "period_end": "2024-09-15T10:30:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.557,
+      "period_end": "2024-09-15T10:00:00.0000000Z",
+      "period": "PT30M"
+    },
+    {
+      "pv_estimate": 1.532,
+      "period_end": "2024-09-15T09:30:00.0000000Z",
+      "period": "PT30M"
+    }
+  ]
+}
+
+radiance_data = {'estimated_actuals': 
+                 [{'ghi': 702, 'ebh': 430, 'dni': 520, 'dhi': 272, 'cloud_opacity': 0, 'period_end': '2024-09-23T05:00:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 595, 'ebh': 291, 'dni': 379, 'dhi': 304, 'cloud_opacity': 6, 'period_end': '2024-09-23T04:30:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 488, 'ebh': 177, 'dni': 255, 'dhi': 311, 'cloud_opacity': 11, 'period_end': '2024-09-23T04:00:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 353, 'ebh': 34, 'dni': 56, 'dhi': 319, 'cloud_opacity': 22, 'period_end': '2024-09-23T03:30:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 276, 'ebh': 17, 'dni': 33, 'dhi': 258, 'cloud_opacity': 22, 'period_end': '2024-09-23T03:00:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 165, 'ebh': 0, 'dni': 0, 'dhi': 165, 'cloud_opacity': 34, 'period_end': '2024-09-23T02:30:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 100, 'ebh': 0, 'dni': 0, 'dhi': 100, 'cloud_opacity': 35, 'period_end': '2024-09-23T02:00:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 52, 'ebh': 0, 'dni': 0, 'dhi': 52, 'cloud_opacity': 25, 'period_end': '2024-09-23T01:30:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 8, 'ebh': 0, 'dni': 0, 'dhi': 8, 'cloud_opacity': 43, 'period_end': '2024-09-23T01:00:00.0000000Z', 'period': 'PT30M'}, 
+                  {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 49, 'period_end': '2024-09-23T00:30:00.0000000Z', 'period': 'PT30M'},
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 64, 'period_end': '2024-09-23T00:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 65, 'period_end': '2024-09-22T23:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 81, 'period_end': '2024-09-22T23:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 82, 'period_end': '2024-09-22T22:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 90, 'period_end': '2024-09-22T22:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 94, 'period_end': '2024-09-22T21:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 94, 'period_end': '2024-09-22T21:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 95, 'period_end': '2024-09-22T20:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 95, 'period_end': '2024-09-22T20:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 94, 'period_end': '2024-09-22T19:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 93, 'period_end': '2024-09-22T19:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 88, 'period_end': '2024-09-22T18:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 66, 'period_end': '2024-09-22T18:00:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 79, 'period_end': '2024-09-22T17:30:00.0000000Z', 'period': 'PT30M'}, 
+                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 72, 'period_end': '2024-09-22T17:00:00.0000000Z', 'period': 'PT30M'},
+                  {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 59, 'period_end': '2024-09-22T16:30:00.0000000Z', 'period': 'PT30M'},
+                    {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 63, 'period_end': '2024-09-22T16:00:00.0000000Z', 'period': 'PT30M'},
+                      {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 78, 'period_end': '2024-09-22T15:30:00.0000000Z', 'period': 'PT30M'},
+                        {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 88, 'period_end': '2024-09-22T15:00:00.0000000Z', 'period': 'PT30M'},
+                          {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 88, 'period_end': '2024-09-22T14:30:00.0000000Z', 'period': 'PT30M'},
+                            {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 93, 'period_end': '2024-09-22T14:00:00.0000000Z', 'period': 'PT30M'},
+                              {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 93, 'period_end': '2024-09-22T13:30:00.0000000Z', 'period': 'PT30M'},
+                                {'ghi': 0, 'ebh': 0, 'dni': 0, 'dhi': 0, 'cloud_opacity': 93, 'period_end': '2024-09-22T13:00:00.0000000Z', 'period': 'PT30M'},
+                                  {'ghi': 21, 'ebh': 0, 'dni': 0, 'dhi': 21, 'cloud_opacity': 40, 'period_end': '2024-09-22T12:30:00.0000000Z', 'period': 'PT30M'}, 
+                                  {'ghi': 56, 'ebh': 0, 'dni': 0, 'dhi': 56, 'cloud_opacity': 42, 'period_end': '2024-09-22T12:00:00.0000000Z', 'period': 'PT30M'}, 
+                                  {'ghi': 55, 'ebh': 0, 'dni': 0, 'dhi': 55, 'cloud_opacity': 70, 'period_end': '2024-09-22T11:30:00.0000000Z', 'period': 'PT30M'},
+                                    {'ghi': 204, 'ebh': 43, 'dni': 92, 'dhi': 161, 'cloud_opacity': 31, 'period_end': '2024-09-22T11:00:00.0000000Z', 'period': 'PT30M'},
+                                      {'ghi': 380, 'ebh': 172, 'dni': 317, 'dhi': 208, 'cloud_opacity': 1, 'period_end': '2024-09-22T10:30:00.0000000Z', 'period': 'PT30M'},
+                                        {'ghi': 478, 'ebh': 240, 'dni': 377, 'dhi': 238, 'cloud_opacity': 0, 'period_end': '2024-09-22T10:00:00.0000000Z', 'period': 'PT30M'},
+                                          {'ghi': 552, 'ebh': 269, 'dni': 376, 'dhi': 283, 'cloud_opacity': 2, 'period_end': '2024-09-22T09:30:00.0000000Z', 'period': 'PT30M'}, 
+                                          {'ghi': 343, 'ebh': 2, 'dni': 3, 'dhi': 340, 'cloud_opacity': 46, 'period_end': '2024-09-22T09:00:00.0000000Z', 'period': 'PT30M'},
+                                            {'ghi': 600, 'ebh': 179, 'dni': 209, 'dhi': 421, 'cloud_opacity': 12, 'period_end': '2024-09-22T08:30:00.0000000Z', 'period': 'PT30M'},
+                                              {'ghi': 655, 'ebh': 227, 'dni': 254, 'dhi': 428, 'cloud_opacity': 9, 'period_end': '2024-09-22T08:00:00.0000000Z', 'period': 'PT30M'}, 
+                                              {'ghi': 758, 'ebh': 406, 'dni': 440, 'dhi': 352, 'cloud_opacity': 0, 'period_end': '2024-09-22T07:30:00.0000000Z', 'period': 'PT30M'}, 
+                                              {'ghi': 777, 'ebh': 426, 'dni': 456, 'dhi': 351, 'cloud_opacity': 0, 'period_end': '2024-09-22T07:00:00.0000000Z', 'period': 'PT30M'}, 
+                                              {'ghi': 780, 'ebh': 437, 'dni': 469, 'dhi': 343, 'cloud_opacity': 0, 'period_end': '2024-09-22T06:30:00.0000000Z', 'period': 'PT30M'}, 
+                                              {'ghi': 740, 'ebh': 375, 'dni': 411, 'dhi': 365, 'cloud_opacity': 3, 'period_end': '2024-09-22T06:00:00.0000000Z', 'period': 'PT30M'},
+                                                {'ghi': 734, 'ebh': 418, 'dni': 476, 'dhi': 316, 'cloud_opacity': 0, 'period_end': '2024-09-22T05:30:00.0000000Z', 'period': 'PT30M'},
+                                                  {'ghi': 651, 'ebh': 311, 'dni': 374, 'dhi': 340, 'cloud_opacity': 5, 'period_end': '2024-09-22T05:00:00.0000000Z', 'period': 'PT30M'}]}
